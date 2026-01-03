f4bd08a81575278ed5771ecdd1a2e3475915527c
Aakash susar <susaraakash4@gmail.com>
2026-01-03 13:17:50 +0530

    Update prediction API endpoint to production URL

--- static/script.js
+++ static/script.js
@@ -68,7 +68,7 @@ async function uploadImage() {
   resultText.innerText = "🔄 Predicting...";
 
   try {
-    const response = await fetch("http://127.0.0.1:5000/predict", {
+    const response = await fetch("https://skincancer-cell-detection.onrender.com/predict", {
       method: "POST",
       body: formData
     });
